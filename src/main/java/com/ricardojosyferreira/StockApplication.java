0fbdd57e748b7bad61a05697cb64e911b23db29e
ricardojosy <ricardo.josy@gmail.com>
2024-07-09 11:35:03 -0300

    feat: rename application class

--- src/main/java/com/ricardojosyferreira/StockApplication.java
+++ src/main/java/com/ricardojosyferreira/StockApplication.java
@@ -3,16 +3,13 @@ package com.ricardojosyferreira;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableRabbit
 @SpringBootApplication
-public class QimaApplication {
+public class StockApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QimaApplication.class, args);
+		SpringApplication.run(StockApplication.class, args);
 	}
 
 }
